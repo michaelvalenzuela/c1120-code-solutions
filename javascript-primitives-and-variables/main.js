94303f68e6545f3604ebea7bf8b6a0affd425527
michaelvalenzuela <20mvalen@gmail.com>
2020-11-16 12:46:47 -0800

    Michael Valenzuela - Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+var fullName = "Michael Valenzuela";
+var isCool = false;
+var totalPets = 2;
+
+console.log("value of fullName:", fullName);
+console.log("value of isCool:", isCool);
+console.log("value of totalPets:", totalPets);
+
+console.log("typeOf fullName:", typeof fullName);
+console.log("typeOf isCool:", typeof isCool);
+console.log("typeOf totalPets:", typeof totalPets);
+
+var unknown = null;
+
+console.log("value of unknown:", unknown);
+console.log("typeof unknown:", typeof unknown);
+
+var never;
+
+console.log("value of never:", never);
+console.log("typeof never:", typeof never);
