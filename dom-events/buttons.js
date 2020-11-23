8526b361b9e4eece8be5778c102e7687be84d24e
michaelvalenzuela <20mvalen@gmail.com>
2020-11-23 12:45:42 -0800

    Michael Valenzuela - Completed dom-events

---
+++ dom-events/buttons.js
@@ -0,0 +1,30 @@
+function handleClick(event){
+  console.log("button clicked");
+  console.log(event);
+  console.log(event.target);
+}
+
+
+let button = document.querySelector(".click-button");
+
+button.addEventListener("click", handleClick);
+
+function handleMouseover(event){
+  console.log("Button Hovered");
+  console.log(event);
+  console.log(event.target);
+}
+
+let hoverButton = document.querySelector(".hover-button");
+
+hoverButton.addEventListener("mouseover", handleMouseover);
+
+function handleDoubleClick(event){
+  console.log("Button Double-Clicked");
+  console.log(event);
+  console.log(event.target);
+}
+
+let dblClickButton = document.querySelector(".double-click-button");
+
+dblClickButton.addEventListener("dblclick", handleDoubleClick);
