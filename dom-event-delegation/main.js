b52dfe52946ba4149202aa26edbb790d26fd1ac9
michaelvalenzuela <20mvalen@gmail.com>
2020-11-30 10:52:05 -0800

    Forgot the remove step

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,5 +3,7 @@ let $taskList = document.querySelector(".task-list");
 $taskList.addEventListener("click", function(e){
   console.log("event.target:", e.target);
   console.log("event.target.tagName:", e.target.tagName);
-  console.log("closest(.task-list-item):", e.target.closest('.task-list-item'));
+  let closest = e.target.closest(".task-list-item");
+  console.log("closest(.task-list-item):", closest);
+  e.target.remove(closest);
 })
