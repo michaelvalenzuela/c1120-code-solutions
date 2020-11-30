b2dc514505867532b7a56545804b56ad19e64e74
michaelvalenzuela <20mvalen@gmail.com>
2020-11-30 11:12:43 -0800

    Added conditional

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,7 +3,9 @@ let $taskList = document.querySelector(".task-list");
 $taskList.addEventListener("click", function(e){
   console.log("event.target:", e.target);
   console.log("event.target.tagName:", e.target.tagName);
-  let closest = e.target.closest(".task-list-item");
-  console.log("closest(.task-list-item):", closest);
-  e.target.remove(closest);
+  if(e.target.tagName === "BUTTON"){
+    let closest = e.target.closest(".task-list-item");
+    console.log("closest(.task-list-item):", closest);
+    closest.remove();
+  }
 })
