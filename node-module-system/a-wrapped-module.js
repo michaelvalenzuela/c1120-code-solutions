e3912b1ddef0e34eb365cd740bfc81a118343f02
michaelvalenzuela <20mvalen@gmail.com>
2020-12-31 15:39:10 -0800

    Michael Valenzuela - Completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log("exports typeof:", typeof module.exports);
+console.log("exports:", module.exports);
+
+console.log("require typeof:", typeof module.require);
+console.log("require:", module.require);
+
+console.log("module typeof:", typeof module);
+console.log("module:", module);
+
+console.log("__filename typeof:", typeof __filename);
+console.log("__filename:", __filename);
+
+console.log("__dirname typeof:", typeof __dirname);
+console.log("__dirname:", __dirname);
