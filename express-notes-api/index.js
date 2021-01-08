8679833b30e0e94bf4e181822451c581c2a6123b
michaelvalenzuela <20mvalen@gmail.com>
2021-01-07 16:56:23 -0800

    Michael Valenzuela - completed express-notes-api

---
+++ express-notes-api/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const data = require('./data');
+const fs = require('fs');
+
+const { notes } = data;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+  const notesArray = [];
+  for (const item in notes) {
+    notesArray.push(notes[item]);
+  }
+  res.status(200).json(notesArray);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    res.status(400).json({ Error: 'ID must be a positive number' });
+  } else if (notes[req.params.id]) {
+    res.status(200).json(notes[req.params.id]);
+  } else {
+    res.status(404).json({ Error: `ID:${req.params.id} does not exist` });
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body;
+  if (!note.content) {
+    res.status(400).json({ Error: 'Content is a required field' });
+  } else {
+    note.id = data.nextId;
+    notes[note.id] = note;
+    data.nextId++;
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+      if (err) {
+        res.status(500).json({ Error: 'An Unexpected error occurred.' });
+        throw err;
+      } else {
+        res.status(201).json(note.id);
+      }
+    });
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    res.status(400).json({ Error: 'id must be a positive integer' });
+  } else if (!notes[req.params.id]) {
+    res.status(404).json({ Error: `ID:${req.params.id} does not exist` });
+  } else {
+    delete notes[req.params.id];
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+      if (err) {
+        res.status(500).json({ Error: 'An Unexpected error occurred.' });
+        throw err;
+      } else {
+        res.status(204).json(notes[req.params.id]);
+      }
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0 || !req.body.content) {
+    res.status(400).json({ Error: 'id must be a positive integer and content is a required field' });
+  } else if (!notes[req.params.id]) {
+    res.status(404).json({ Error: `ID:${req.params.id} does not exist` });
+  } else {
+    notes[req.params.id].content = req.body.content;
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+      if (err) {
+        res.status(500).json({ Error: 'An Unexpected error occurred.' });
+        throw err;
+      } else {
+        res.status(200).json(notes[req.params.id]);
+      }
+    });
+  }
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is listening on port 3000');
+});
